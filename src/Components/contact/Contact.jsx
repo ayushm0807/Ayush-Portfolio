31eb3e526bc49b8eaf7057eb407316a34f3193f5
Ayush Mishra <ayushm0807@users.noreply.github.com>
2025-02-24 18:29:04 +0530

    Update Contact

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -42,7 +42,7 @@ const Contact = () => {
               <MdOutlineEmail className='contact__option-icon' />
               <h4>Email</h4>
               <h5>ayushm0807@gmail.com</h5>
-              <a href="mailto:ayushmishra0807@gmail.com/" target='_blank'> Send a Message</a>
+              <a href="mailto:ayushm0807@gmail.com" target='_blank'> Send a Message</a>
             </article>
 
             <article className="contact__option">
